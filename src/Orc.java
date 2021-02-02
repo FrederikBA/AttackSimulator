b0519fdb8668b1e08eb287de172d8f1a46887933
FrederikBA <54848880+FrederikBA@users.noreply.github.com>
2021-02-02 13:02:30 +0100

    Added Orc class

---
+++ src/Orc.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+
+public class Orc implements Attack {
+    private String name;
+    private Random rd;
+
+    public Orc(String name) {
+        this.name = name;
+        rd = new Random();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int monsterAttack() {
+        int strength = 1 + rd.nextInt(44);
+        return strength;
+    }
+
+    @Override
+    public String toString() {
+        return name + " Strength: " + monsterAttack();
+    }
+}
+
